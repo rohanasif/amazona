8c89a86de50be4b6c04859437028fdbcbd4a6c47
rohanasif <rohanasif1990@gmail.com>
2024-01-25 13:10:26 +0500

    productRoutes works

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,23 @@
 import express from "express";
-import products from "./data/products.js"
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from "colors";
+import productsRoutes from "./routes/productRoutes.js";
 
+const app = express();
+dotenv.config();
+connectDB();
 
-const app = express()
-dotenv.config()
-connectDB()
+app.get("/", (req, res) => {
+  res.send("api is running");
+});
 
-app.get('/', (req, res) => {
-    res.send("api is running")
-})
-app.get('/api/products', (req, res) => {
-    res.send(products)
-})
+app.use("/api/products", productsRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id == req.params.id)
-    res.json(product)
-})
-
-const port = process.env.PORT
+const port = process.env.PORT;
 
 app.listen(port, () => {
-    console.log (`Server is running in ${process.env.MODE} mode on port ${port}`.yellow.bold);
-})
+  console.log(
+    `Server is running in ${process.env.MODE} mode on port ${port}`.yellow.bold
+  );
+});
