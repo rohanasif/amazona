33f6071f81ff3d5406cfdd2117cb3d4d17a41cc9
rohanasif <rohanasif1990@gmail.com>
2024-01-26 10:25:25 +0500

    add middleware

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./config/db.js";
+import dbConnection from "./congiguration/dbConnect.js";
 import colors from "colors";
-import productsRoutes from "./routes/productRoutes.js";
+import productRoutes from "./Routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+console.log(colors, "heloo coors");
 
 const app = express();
+
+app.use(express.json());
 dotenv.config();
-connectDB();
+dbConnection();
+app.use("/api/products", productRoutes);
 
 app.get("/", (req, res) => {
-  res.send("api is running");
+  res.json("API is running");
 });
 
-app.use("/api/products", productsRoutes);
+const PORT = process.env.PORT || 8002;
 
-const port = process.env.PORT;
+app.use(notFound);
+app.use(errorHandler);
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(
-    `Server is running in ${process.env.MODE} mode on port ${port}`.yellow.bold
+    `server is running on ${process.env.NODE_ENV} mode on port ${PORT}.rainbow.bold`
   );
 });
